82fd696e74964dd83d82725904ce15ff8b968f4c
Derek Johnson <wdjcodes@gmail.com>
2025-04-03 02:59:02 +0100

    feat: add constrain and filter to knex QueryBuilder interface
    initial setup of library with types and minimal functions
    to verify that library is on the right path

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import knex, { Knex } from "knex";
+
+export function attachConstraints(){
+  knex.QueryBuilder.extend("constrain", constrain);
+  knex.QueryBuilder.extend("filter", filter);
+}
+
+function constrain<TRecord extends {} = any, TResult = any>(
+  this: Knex.QueryBuilder<TRecord, TResult>,
+  constraint: Constraint
+) {
+  if (constraint.filter) {
+    this.filter(constraint.filter);
+  }
+
+  return this;
+}
+
+function filter<TRecord extends {} = any, TResult = any>(
+  this: Knex.QueryBuilder<TRecord, TResult>,
+  filter: FilterConstraint
+) {
+  for (const k in filter) {
+    let c = filter.k;
+    if (c.eq) {
+      this.where(k, "=", c.eq);
+    }
+  }
+
+  return this;
+}
+
+export type Constraint = {
+  filter?: FilterConstraint;
+};
+
+export type FilterConstraint = {
+  [k: string]: ConstraintOperator;
+};
+
+type ConstraintOperator = EqConstraint;
+
+type EqConstraint = {
+  eq: string | number;
+};
+
+declare module "knex" {
+  namespace Knex {
+    interface QueryBuilder<TRecord extends {} = any, TResult = any> {
+      constrain(
+        constraint: Constraint
+      ): Knex.QueryBuilder<TRecord, TResult>;
+      filter(
+        filter: FilterConstraint
+      ): Knex.QueryBuilder<TRecord, TResult>;
+    }
+  }
+}
